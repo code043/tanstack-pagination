9cb0d91a6163a74eb1908670579bc8f20b19e419
code043 <procoder043@gmail.com>
2024-10-04 16:43:42 -0300

    Init

---
+++ src/app/utils/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { Post } from "../types/Post";
+import { User } from "../types/User";
+
+const req = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+export const getPosts = async (
+  limit: number = 10,
+  start: number = 0
+): Promise<Post[]> => {
+  const result = await req.get(`/posts?_limit=${limit}&_start=${start}`);
+  return result.data;
+};
+
+export const getPost = async (id: number): Promise<Post> => {
+  const result = await req.get("/posts/" + id);
+  return result.data;
+};
+
+export const getUsers = async (): Promise<User[]> => {
+  const result = await req.get("/users");
+  return result.data;
+};
